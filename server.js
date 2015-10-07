1c5a9504fc2634e4b14f3a6473369b374a3c66d5
cwyeth <corinne.wyeth@yale.edu>
2015-10-07 02:05:31 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,4 +5,12 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 
-app.listen(process.env.PORT || 4000);
+app.get('/nickname', function(request, response){
+    response.send('friendly-dove');
+});
+
+
+app.listen(
+    process.env.PORT || 4000,
+    process.env.IP || 'localhost'
+    );
